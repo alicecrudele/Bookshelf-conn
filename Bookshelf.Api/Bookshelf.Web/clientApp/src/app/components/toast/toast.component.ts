6493fefb1d6148a6b5b69e470244036e72e3ed2d
Alice Crudele <alice.crudele@beantech.it>
2023-11-17 17:17:03 +0100

    added toast component things

--- Bookshelf.Api/Bookshelf.Web/clientApp/src/app/components/toast/toast.component.ts
+++ Bookshelf.Api/Bookshelf.Web/clientApp/src/app/components/toast/toast.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class ToastComponent {
 
+  //@Input() public type: ToastType;
+  //@Input() public message: string;
+  //@Input() public popupRef: PopupRef;
+  //public closeEvent = new EventEmitter();
+  //public toastType = ToastType;
+
+  ///*Labels*/
+  //public labelToastError: string;
+  //public labelToastWarning: string;
+  //public labelToastInfo: string;
+
+  //constructor(
+  //  private commonSvc: CommonService,
+  //  private sessionSvc: SessionService,
+  //  private constantHelper: ConstantHelper
+  //) {
+
+  //  this.commonSvc.initializeLabels().then(
+  //    result => {
+  //      this.labelToastError = sessionSvc.getLabel(constantHelper.LBL_TOAST_ERROR);
+  //      this.labelToastWarning = sessionSvc.getLabel(constantHelper.LBL_TOAST_WARNING);
+  //      this.labelToastInfo = sessionSvc.getLabel(constantHelper.LBL_TOAST_INFO);
+  //    },
+  //    error => {
+  //      console.error(error);
+  //    });
+  //}
+
+  //public close(e: any) {
+  //  this.closeEvent.emit(this.popupRef);
+  //}
+
+
+
 }
